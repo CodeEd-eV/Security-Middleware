c61cf772c7770197bb268a6639792ceb0c82d935
Tobias Urban <turban2009@live.de>
2018-12-09 07:18:19 +0100

    Fixed login error in callback

--- Routes/ActiveDirectory/callback.js
+++ Routes/ActiveDirectory/callback.js
@@ -17,7 +17,7 @@ exports.act = (req,res) => {
                 grant_type: 'authorization_code',
                 code: code,
                 client_secret: process.env.CLIENT_SECRET,
-                redirect_uri: 'http://localhost:3000/GET/ad/callback',
+                redirect_uri: 'https://stscodeed.azurewebsites.net/GET/ad/callback',
                 client_id: process.env.CLIENT_ID
             }
         };
